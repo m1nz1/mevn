215294749c56177799860042c3c9ba250550d003
zoselaslwl <smj980902@gmail.com>
2023-07-03 17:36:18 +0900

    20230703

---
+++ note/p000/ex04.promise_catch예제3.js
@@ -0,0 +1,20 @@
+/* 일반적인 에러 캐치 상황 */
+function testError(){
+    console.log('에러 던지기를 시작합니다.')
+    throw new Error('testError에서 에러를 고의 발색 시켰습니다.')
+}
+
+function main(){
+    try{
+        console.log('=====try의 시작=====')
+        testError()
+        console.log('====try의 끝=====')
+    }catch(e) {
+        console.log('=====catch의 시작====')
+        console.log('=====catch의 끝====')
+    }finally{
+        console.log('====finally의 시작=====')
+        console.log('에러 테스트를 종료합니다.')
+        console.log('====finally의 끝=====')
+    }
+}main()
---
+++ note/tip/t02_request_공공API_주민등록.js
@@ -0,0 +1,10 @@
+const request = require('request')
+
+const url =  'http://apis.data.go.kr/6260000/BusanPopulationStaticService/getPopulationInfo?serviceKey='
+
+// request(url,(e,res,body)=>{
+//     const rst = JSON.parse(body)
+//     const jonpo = rst.response.body.items
+//     junpo.forEach(v,(i)=>{
+       
+// })
